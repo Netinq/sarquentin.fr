478449789d92674f97ac18f03461dfeb9fd17b60
BuildTools <sarquentin@gmail.com>
2022-11-05 17:35:29 +0100

    :bug: Fixing trust proxy

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = [
+        '0.0.0.0/0',
+    ];
 
     /**
      * The headers that should be used to detect proxies.
