da6de1d6f46f026e4b0ccade17cb07059f918cdc
BuildTools <sarquentin@gmail.com>
2022-11-29 21:45:07 +0100

    :sparkles: Trying upgrade perf

--- resources/views/home/feedback.blade.php
+++ resources/views/home/feedback.blade.php
@@ -7,7 +7,7 @@
     <div class="container">
         @foreach($reviews as $review)
         <div class="review" itemscope itemtype="https://schema.org/Review">
-            <img class="authorImg" src="{{ $review[0][2] }}" alt="" />
+            <img class="authorImg" loading="lazy" src="{{ $review[0][2] }}" alt="" />
             <meta itemprop="author" content = "{{$review[0][1]}}">
             <div class="note" itemprop="reviewRating" itemscope itemtype="https://schema.org/Rating">
                 <meta itemprop="ratingValue" content = "{{$review[4]}}">
