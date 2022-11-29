0a1260f201033ae29da371898910288b0cb33b88
BOOK-QU8VNSMR86\sarqu <comptes@sarquentin.fr>
2022-11-29 01:25:24 +0100

    :bug: Fixing responsive

--- resources/js/feedback.js
+++ resources/js/feedback.js
@@ -1,18 +1,23 @@
 const reviews = document.querySelectorAll('.review');
 const next = document.querySelector('#next');
-let displayedReviews = [0, 1, 2]
 
-if (window.innerWidth >= 991) displayedReviews = [0, 1]
+let displayedReviews;
+
+if (window.innerWidth >= 1200) displayedReviews = [0, 1, 2]
+else if (window.innerWidth >= 991) displayedReviews = [0, 1]
 else displayedReviews = [0]
 
 Array.from(reviews).forEach((review, index) => {
     const reviewBody = review.querySelector('p.reviewBody');
     const reviewAuthor = review.querySelector('h4');
-    if (reviewAuthor.textContent.length > 30) reviewAuthor.innerHTML = redureAuthor(reviewAuthor.textContent);
+    if (reviewAuthor.textContent.length > 30 && window.innerWidth >= 991) reviewAuthor.innerHTML = redureAuthor(reviewAuthor.textContent);
+    else if (reviewAuthor.textContent.length > 20) reviewAuthor.innerHTML = redureAuthor(reviewAuthor.textContent);
     reviewBody.innerHTML = reduceText(reviewBody.innerHTML);
     const max = window.innerWidth >= 1200 ? 3 : window.innerWidth >= 991 ? 2 : 1;
     if (index >= max) {
         review.style.display = 'none';
+    } else {
+        review.style.display = 'flex';
     }
 });
 
@@ -23,13 +28,19 @@ function nextReviews() {
     if (window.innerWidth >= 1200) {
         if (displayedReviews[2] + max <= reviews.length - 1) displayedReviews[2] += max;
         else displayedReviews[2] = 2 - ((reviews.length - 1) - displayedReviews[2]);
+        if (displayedReviews[1]+max <= reviews.length - 1) displayedReviews[1] +=max ;
+        else displayedReviews[1] = 2 - ((reviews.length - 1) - displayedReviews[1]);
+        if (displayedReviews[0]+max <= reviews.length - 1) displayedReviews[0] +=max;
+        else displayedReviews[0] = 2 - ((reviews.length - 1) - displayedReviews[0]);
     } else if (window.innerWidth >= 991) {
         if (displayedReviews[1]+max <= reviews.length - 1) displayedReviews[1] +=max ;
         else displayedReviews[1] = 2 - ((reviews.length - 1) - displayedReviews[1]);
+        if (displayedReviews[0]+max <= reviews.length - 1) displayedReviews[0] +=max;
+        else displayedReviews[0] = 2 - ((reviews.length - 1) - displayedReviews[0]);
     } else {
         if (displayedReviews[0]+max <= reviews.length - 1) displayedReviews[0] +=max;
         else displayedReviews[0] = 2 - ((reviews.length - 1) - displayedReviews[0]);
-    }
+
     console.log(displayedReviews);
     displayReviews();
 }
@@ -49,8 +60,10 @@ function displayReviews() {
 }
 
 function reduceText(text) {
-    if (text.length > 150) {
+    if (text.length > 150 && window.innerWidth >= 991) {
         return text.substring(0, 100) + '...';
+    } else if (text.length > 150) {
+        return text.substring(0, 75) + '...';
     } else {
         return text;
     }
